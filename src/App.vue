8e89f658d75087018cd540efe18236f8c514ba78
ClydeFrog <dvennegeerts@gmail.com>
2020-03-11 17:55:05 +0100

    added two textareas reformatting and linting

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,59 @@
 <template>
   <div id="app">
-    
-      <h1>Hi! Wie fühlst du dich heute?</h1>
-   <div class="container">
-      <div class="basic-grid">
-        <div  v-for="(mood, index) in moods" :key="index">
-          <input type="radio" :value="mood.value" v-model="checked" />
-          <label :for="mood">{{ mood.text }}</label>
-        </div>
+    <h1>Hi! Wie fühlst du dich heute?</h1>
+    <texts v-model="what" :title="'Was hat dich bedrückt?'"></texts>
+    <p>
+      {{ what.txt }}
+    </p>
+    <texts v-model="how" :title="'Was könntest du daran ändern?'"></texts>
+    <p>
+      {{ how.txt }}
+    </p>
+    <div class="basic-grid">
+      <div v-for="(mood, index) in moods" :key="index">
+        <input type="radio" :value="mood.value" v-model="checked" />
+        <label :for="mood">{{ mood.text }}</label>
       </div>
-      <br />
       <span>Checked names: {{ checked }}</span>
     </div>
-    <text-what-worries-me 
-    v-model="name"
-    :title="'Was hat dich bedrückt?'"/>
-    <text-what-worries-me 
-    :title="'Was könntest du daran ändern?'"/>
-    aaaaaaaaaaaaaaaaaaaaaaaaaaaa
-    {{name}}
-   
-    
-
-    
   </div>
 </template>
 
 <script>
-import TextWhatWorriesMe from './components/TextWhatWorriesMe.vue';
+import texts from "./components/texts.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    TextWhatWorriesMe,
+    texts
   },
   data() {
     return {
-      checked:'',
-      text:'',
-      name:''
+      checked: "",
 
-    }
+      what: {
+        txt: ""
+      },
+      how: {
+        txt: ""
+      }
+    };
   },
-  computed:{
-      moods() {
+  computed: {
+    moods() {
       let moodArr = [
         { text: "🙂 Es eigentlich ganz okay", value: 1 },
         { text: "😑 Es nervt mich schon", value: 2 },
         { text: "😱  was soll der Kack?!", value: 3 }
       ];
       return moodArr;
-    },
+    }
     // selected(){
     //   let selected = '';
     //   return selected;
     // }
-    
   }
-}
+};
 </script>
 
 <style>
@@ -70,8 +66,8 @@ export default {
   margin-top: 60px;
 }
 
-.basic-grid{
-  display:grid;
+.basic-grid {
+  display: grid;
   gap: 1rem;
 
   grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
