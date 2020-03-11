895c51c2dea76bfa89d6ef8f97dee0899f71a867
ClydeFrog <dvennegeerts@gmail.com>
2020-03-11 17:18:06 +0100

    skeleton mood diary

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    
+      <h1>Hi! Wie fühlst du dich heute?</h1>
+   <div class="container">
+      <div class="basic-grid">
+        <div  v-for="(mood, index) in moods" :key="index">
+          <input type="radio" :value="mood.value" v-model="checked" />
+          <label :for="mood">{{ mood.text }}</label>
+        </div>
+      </div>
+      <br />
+      <span>Checked names: {{ checked }}</span>
+    </div>
+    <text-what-worries-me 
+    v-model="name"
+    :title="'Was hat dich bedrückt?'"/>
+    <text-what-worries-me 
+    :title="'Was könntest du daran ändern?'"/>
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    {{name}}
+   
+    
+
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import TextWhatWorriesMe from './components/TextWhatWorriesMe.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld
+    TextWhatWorriesMe,
+  },
+  data() {
+    return {
+      checked:'',
+      text:'',
+      name:''
+
+    }
+  },
+  computed:{
+      moods() {
+      let moodArr = [
+        { text: "🙂 Es eigentlich ganz okay", value: 1 },
+        { text: "😑 Es nervt mich schon", value: 2 },
+        { text: "😱  was soll der Kack?!", value: 3 }
+      ];
+      return moodArr;
+    },
+    // selected(){
+    //   let selected = '';
+    //   return selected;
+    // }
+    
   }
-};
+}
 </script>
 
 <style>
@@ -24,5 +68,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.basic-grid{
+  display:grid;
+  gap: 1rem;
+
+  grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
 }
 </style>
---
+++ src/components/TextWhatWorriesMe.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>{{title}}</h1>
+
+      <textarea  rows="4" cols="50" v-model="text"> </textarea>
+
+      <pre class="mt-3 mb-0">{{ text }}</pre>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TextWhatWorriesMe",
+  props:{
+      title:{
+          type: String,
+          required:true,
+      }
+  },
+  data() {
+    return {
+      text: ""
+    };
+  },
+  computed:{
+  }
+};
+</script>
